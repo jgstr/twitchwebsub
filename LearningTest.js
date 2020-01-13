69bde02ecd61461a5a3cbc3b6d833bb285960aa4
Aaron <aaron@aarongoold.com>
2020-01-12 17:00:25 -0800

    Created Node project. Sent request to Twitch's sample endpoint

---
+++ LearningTest.js
@@ -0,0 +1,49 @@
+// NodeJS App to:
+// 1. Send HTTP Request to Twitch to register subscription
+// 2. Handle all Responses and Requests (notifications) from Twitch 
+// (ie. If Twitch sends a POST and expects a 200 response, I should fulfill that)
+
+//Load HTTP module
+const http = require("http");
+const https = require('https')
+const hostname = '127.0.0.1';
+const port = 9000;
+
+//Create HTTP server and listen on port 3000 for requests
+const server = http.createServer((req, res) => {
+
+  //Set the response HTTP header with HTTP status and Content type
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+//listen for request on port 9000, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// This makes the same call to the dummy/sample stream shown on the Twitch Getting Started page.
+// Next: Use this as a starting point to send a GET Request to a subscription Webhook.
+const options = {
+  hostname: 'api.twitch.tv',
+  path: '/helix/streams?game_id=33214',
+  method: 'GET',
+  headers: {
+      'Client-ID': 'cqsp04vus3gbjwp54lfcxiz90xmbix'
+  }
+}
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.end()
