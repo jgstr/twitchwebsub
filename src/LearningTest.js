e5d7c67a4e255e8bf123eea18700cb1625b4c84d
Aaron <aaron@aarongoold.com>
2020-01-14 18:42:17 -0800

    Added clientID to gitignored references directory

--- src/LearningTest.js
+++ src/LearningTest.js
@@ -3,26 +3,19 @@
 // 2. Handle all Responses and Requests (notifications) from Twitch 
 // (ie. If Twitch sends a POST and expects a 200 response, I should fulfill that)
 
-
-/*
-
-
-//Load HTTP module
+import {clientID} from "../references/authentications";
 const http = require("http");
-const https = require('https')
+const https = require('https');
+
 const hostname = '127.0.0.1';
 const port = 9000;
 
-//Create HTTP server and listen on port 9000 for requests
 const server = http.createServer((req, res) => {
-
-  //Set the response HTTP header with HTTP status and Content type
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
 
-//listen for request on port 9000, and as a callback function have the port listened on logged
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
@@ -35,22 +28,20 @@ const options = {
   path: '/helix/streams?game_id=33214',
   method: 'GET',
   headers: {
-      'Client-ID': '8b4pq8s959veuk81vwpg9yxl7nsggw'
+      'Client-ID': clientID
   }
 }
 
 const req = https.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`)
+  console.log(`statusCode: ${res.statusCode}`);
 
   res.on('data', d => {
-    process.stdout.write(d)
-  })
-})
+    process.stdout.write(d);
+  });
+});
 
 req.on('error', error => {
-  console.error(error)
-})
+  console.error(error);
+});
 
 req.end();
-
-*/
