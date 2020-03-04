3ca27b58bc9ec593f67bf71f33bb1158a18f1de1
Aaron <aaron@aarongoold.com>
2020-03-03 18:29:15 -0800

    Add data-store file and data-store-integration- test file.

---
+++ subscriber/tests/data-store-integration-test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+describe('Data Store', function() {
+  it('should return a list of subscriptions.', function() {
+    const expectedValue = {};
+    expect(subscriptions).to.equal(expectedValue);
+  });
+});
