748264616dd978fd7fb7b07e37e8209d5fbb316f
Aaron <aaron@aarongoold.com>
2020-03-03 18:52:50 -0800

    Refactored data-store-integration-test a little. Fishing for most accurate expectations.

--- subscriber/tests/data-store-integration-test.js
+++ subscriber/tests/data-store-integration-test.js
@@ -1,8 +1,14 @@
 import { expect } from 'chai';
+import { createDataStore } from '../data-store';
 
 describe('Data Store', function() {
+
   it('should return a list of subscriptions.', function() {
-    const expectedValue = {};
-    expect(subscriptions).to.equal(expectedValue);
+    const dataStore = createDataStore(pool);
+    const expectedValue = {
+      subscriptionId: 1
+    };
+    expect(subscriptions).to.deep.equal(expectedValue);
   });
+
 });
