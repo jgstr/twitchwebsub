e554a87649a96cb6f7e0c0ed409c016a1688e729
Aaron <aaron@aarongoold.com>
2020-03-05 20:18:01 -0800

    Added before to data-store-integration test. But the compose import is always undefined. What's more, docker-compose has strange side effects when I attempt to run it in the command line from the subscriber directory.

--- subscriber/tests/data-store-integration-test.js
+++ subscriber/tests/data-store-integration-test.js
@@ -1,9 +1,18 @@
+import path from 'path';
 import { expect } from 'chai';
 import { createDataStore } from '../data-store';
+import compose from 'docker-compose';
 
-describe('Data Store', function() {
+describe('Data Store', function () {
 
-  it('should return a list of subscriptions.', function() {
+  before(function () {
+    return compose.upAll({cwd: path.join(__dirname, '..'), log: true})
+    .then(() => {
+      console.log('* docker ran.');
+    });
+  });
+
+  it('should return a list of subscriptions.', function () {
     const dataStore = createDataStore(pool);
     const expectedValue = {
       subscriptionId: 1
