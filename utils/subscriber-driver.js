9c6ac5fc5a5792ccbbd6409291aab3f85e75f190
Aaron <aaron@aarongoold.com>
2020-03-13 16:39:10 -0700

    Added removeSubscription to suscriber-driver

--- utils/subscriber-driver.js
+++ utils/subscriber-driver.js
@@ -3,5 +3,6 @@ const axios = require('axios');
 const getAllSubscriptions = () => { return axios.get('http://localhost:3000/get-subscriptions'); }
 const requestSubscription = () => { return axios.get('http://localhost:3000/subscribe'); }
 const getAllEvents = () => { return axios.get('http://localhost:3000/get-events'); }
+const removeSubscription = () => { return axios.get('http://localhost:3000/unsubscribe'); };
 
-module.exports = { getAllSubscriptions, requestSubscription, getAllEvents };
+module.exports = { getAllSubscriptions, requestSubscription, getAllEvents, removeSubscription };
