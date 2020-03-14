9c9209bbd93532cee64b30ae96f3915a50506180
Aaron <aaron@aarongoold.com>
2020-03-13 17:22:08 -0700

    Added the action to trigger the feature I want to test for removing subscriptions.

--- tests/subscriber-tests.js
+++ tests/subscriber-tests.js
@@ -1,5 +1,15 @@
-describe('Subscriber Server', function(){
-  it('', function(){
+const subscriber = require('../utils/subscriber-driver');
 
+describe('Subscriber Server', function () {
+
+  before(function(done){
+    subscriber.start(subscriberApp)
+    .then(done);
+  });
+
+  // Note: 2nd end-to-end tests should have gone here, but I accidentally skipped that step.
+
+  it('should remove one subscription.', function () {
+    subscriber.removeSubscription(subscription);
   });
 });
