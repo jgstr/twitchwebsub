3db2ecd8929432ab3cd63cd5a283f2db3cef3e48
Aaron <aaron@aarongoold.com>
2020-04-11 11:11:34 -0700

    Updated env-writer-production to include ngrok public url.

--- utils/dot-env-writer-production.js
+++ utils/dot-env-writer-production.js
@@ -1,9 +1,17 @@
 const fileSystem = require('fs');
-const ngrok = require('./get-ngrok-url');
+const axios = require('axios');
 
-const callback = ngrok.getPublicUrl(); //TODO: Gets undefined. Figure out this next.
-const hub = 'hub_url_env=https://api.twitch.tv/helix/webhooks/hub';
 
-fileSystem.writeFile('.env', `${callback}\n${hub}`, function(err){
-  if (err) return console.error(err);
-});
+axios.get('http://localhost:4040/api/tunnels')
+.then(res => {
+  const callback = 'hub_callback_env=' + res.data.tunnels[0]['public_url'];
+  const hub = 'hub_url_env=https://api.twitch.tv/helix/webhooks/hub';
+  
+  fileSystem.writeFile('.env', `${callback}\n${hub}`, function(err){
+    if (err) return console.error(err);
+  });
+
+})
+.catch(err => console.log('Did you start ngrok?'));
+
+
--- utils/get-ngrok-url.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-const getPublicUrl = () => {
-  axios.get('http://localhost:4040/api/tunnels')
-  .then(res => res.data.tunnels[0]['public_url'])
-  .catch(err => console.log('Did you start ngrok?'));
-};
-
-module.exports = { getPublicUrl };
