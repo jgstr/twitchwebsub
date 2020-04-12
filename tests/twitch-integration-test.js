86bc5b1971ed49d0761ca8bc218c0e7c150de814
Aaron <aaron@aarongoold.com>
2020-04-12 16:02:33 -0700

    Add todo for next step.

--- tests/twitch-integration-test.js
+++ tests/twitch-integration-test.js
@@ -1,14 +1,16 @@
 import { expect } from "chai";
 const twitch = require("../subscriber/adapters/twitch");
 const subscriber = require("../subscriber/subscriber");
-import { subscriptionRecordStub } from "../subscriber/doubles/subscriptions";
+import { subscriptionStub } from "../subscriber/doubles/subscriptions";
 
 describe("Twitch", function () {
   const twitchAdapter = twitch.createTwitchAdapter();
 
+  // TODO: this test might need nock or to run fake-twitch to respond to the twitch adapter axios request.
+
   it("should send a subscription request.", function () {
     subscriber
-      .requestSubscription(twitchAdapter, subscriptionRecordStub)
+      .requestSubscription(twitchAdapter, subscriptionStub)
       .then((response) => {
         expect(response).to.equal("Received");
       });
