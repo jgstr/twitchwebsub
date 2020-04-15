8ffc581b582ce1bf876027d99739cbd45b2f35f8
Aaron <aaron@aarongoold.com>
2020-04-15 11:26:21 -0700

    Just removed a comment. Commiting to change branches.

--- subscriber/adapters/twitch.js
+++ subscriber/adapters/twitch.js
@@ -4,12 +4,9 @@ export const createTwitchAdapter = () => {
   return {
     requestSubscription: (subscription) => {
       return new Promise((resolve, reject) => {
-        // const isLocalDevelopment = subscription.hubUrl.includes('localhost');
-
         axios({
           method: "POST",
           url: subscription.hubUrl,
-          // url: (isLocalDevelopment ? subscription.hubUrl : 'https://api.twitch.tv/helix/webhooks/hub'),
           headers: {
             "Content-Type": "application/json",
             "Client-ID": subscription.clientID,
