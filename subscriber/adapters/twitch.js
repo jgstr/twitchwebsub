19634e0a91274e53ec131e84716c5121706dd4a5
Aaron <aaron@aarongoold.com>
2020-04-22 14:17:14 -0700

    Created stub createHubTopicURL helper function for twitch adapter.

--- subscriber/adapters/twitch.js
+++ subscriber/adapters/twitch.js
@@ -1,5 +1,9 @@
 const axios = require("axios");
 
+const createHubTopicURL = (topicType, toID, fromID, userID) => {
+  return "https://api.twitch.tv/helix/users/follows?first=1&to_id=17337557";
+};
+
 export const createTwitchAdapter = (twitchHub) => {
   return {
     requestSubscription: (subscription) => {
@@ -14,7 +18,13 @@ export const createTwitchAdapter = (twitchHub) => {
           data: {
             "hub.callback": subscription.hubCallback,
             "hub.mode": "subscribe",
-            "hub.topic": subscription.topic, // createHubTopicURL(topicType, to, from, userID)
+            // "hub.topic": subscription.topic,
+            "hub.topic": createHubTopicURL(
+              subscription.topic,
+              subscription.toID,
+              subscription.fromID,
+              subscription.userID
+            ),
             "hub.lease_seconds": 600,
           },
         })
