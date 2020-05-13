eff4475ac2aebfe69db818796fe77dedbe6b3328
Aaron <aaron@aarongoold.com>
2020-05-13 09:57:10 -0700

    Fixed bug in get-twitch-live-streams. Replace client secret with appAccessToken.

--- utils/get-twitch-live-streams.js
+++ utils/get-twitch-live-streams.js
@@ -1,5 +1,9 @@
 const axios = require("axios");
-import { clientID, clientSecret } from "../references/authentications";
+import {
+  appAccessToken,
+  clientID,
+  clientSecret,
+} from "../references/authentications";
 
 const daysTillTokenExpiry = () => {
   const millisInDay = 1000 * 60 * 60 * 24;
@@ -14,16 +18,19 @@ const getTwitchLiveStreams = () => {
     url: "https://api.twitch.tv/helix/streams",
     headers: {
       "Client-ID": clientID,
-      Authorization: `Bearer ${clientSecret}`,
+      Authorization: `Bearer ${appAccessToken}`,
     },
   })
     .then((streams) => {
-      console.log("* Days until token expiry: about ", daysTillTokenExpiry());
+      console.log(
+        "* Days until App Access Token expiry: about ",
+        daysTillTokenExpiry()
+      );
       console.log("* Twitch streams: ", streams.data.data);
     })
     .catch((err) => {
       console.log(
-        "* Check that your token is valid.\n* Days until expiry: about ",
+        "* Check that your OAuth is valid.\n* Days until App Access Token expiry: about ",
         daysTillTokenExpiry()
       );
       console.error(
