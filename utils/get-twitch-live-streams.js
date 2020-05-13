78231414e99767b6fdb6a0d7acf8d3a3a327f23e
Aaron <aaron@aarongoold.com>
2020-05-13 09:46:13 -0700

    Added OAuth token expiration date warning to get-twitch-live-streams.

--- utils/get-twitch-live-streams.js
+++ utils/get-twitch-live-streams.js
@@ -1,6 +1,13 @@
 const axios = require("axios");
 import { clientID, clientSecret } from "../references/authentications";
 
+const daysTillTokenExpiry = () => {
+  const millisInDay = 1000 * 60 * 60 * 24;
+  const tokenExpiry = new Date(2020, 6, 7); // July 7, 2020
+  const millisTillExpiry = tokenExpiry - Date.now();
+  return Math.floor(millisTillExpiry / millisInDay);
+};
+
 const getTwitchLiveStreams = () => {
   axios({
     method: "GET",
@@ -10,8 +17,20 @@ const getTwitchLiveStreams = () => {
       Authorization: `Bearer ${clientSecret}`,
     },
   })
-    .then((streams) => console.log("* Twitch streams: ", streams.data.data))
-    .catch((err) => console.error("* get-twitch-live-streams error: ", err));
+    .then((streams) => {
+      console.log("* Days until token expiry: about ", daysTillTokenExpiry());
+      console.log("* Twitch streams: ", streams.data.data);
+    })
+    .catch((err) => {
+      console.log(
+        "* Check that your token is valid.\n* Days until expiry: about ",
+        daysTillTokenExpiry()
+      );
+      console.error(
+        "* get-twitch-live-streams error: ",
+        err.response.data.message
+      );
+    });
 };
 
 getTwitchLiveStreams();
