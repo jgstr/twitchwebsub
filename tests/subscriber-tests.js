4e6516c06afb816abaa04751b8ed5e77e1d11688
Aaron <jgstr@users.noreply.github.com>
2020-07-14 12:00:43 -0700

    Refactored subscriber-tests to pass with new web wiring.

--- tests/subscriber-tests.js
+++ tests/subscriber-tests.js
@@ -1,4 +1,4 @@
-import subscriber from "../subscriber/subscriber";
+import { createSubscriberManager } from "../subscriber/subscriber";
 import { expect } from "chai";
 const dataStoreFake = require("../subscriber/doubles/data-store-fake");
 const twitchStub = require("../subscriber/doubles/twitch-stub");
@@ -9,28 +9,33 @@ import {
   subscriptionRecordStub,
 } from "../subscriber/doubles/subscriptions";
 
+// Might need to move this to it's own module eventually.
+const twitchDummy = { requestSubscription: () => Promise.resolve("Received") };
+const subscriberManager = createSubscriberManager(dataStoreFake, twitchDummy);
+
+console.log("Subscriber Manager: ", subscriberManager);
+
 describe("Subscriber Server", function () {
   it("should return a list of subscriptions.", function () {
-    const subscriptions = subscriber.getAllSubscriptions(dataStoreFake);
+    const subscriptions = subscriberManager.getAllSubscriptions();
     expect(subscriptions).to.include.deep.members([subscriptionRecordStub]);
   });
 
   it("should send a subscription request.", function () {
-    subscriber
-      .requestSubscription(twitchStub, subscriptionDummy)
+    subscriberManager
+      .requestSubscription(subscriptionDummy)
       .then((response) => {
         expect(response).to.equal("Received");
       });
   });
 
   it("should save a subscription.", function () {
-    subscriber.saveSubscription(dataStoreFake, subscriptionRecordStub);
+    subscriberManager.saveSubscription(subscriptionRecordStub);
     expect(dataStoreFake.subscriptionDatabase.size).to.equal(1);
   });
 
   it("should return one subscription.", function () {
-    const subscription = subscriber.getSubscription(
-      dataStoreFake,
+    const subscription = subscriberManager.getSubscription(
       subscriptionRecordStub
     );
     expect(subscription).to.deep.equal(subscriptionRecordStub);
@@ -41,29 +46,21 @@ describe("Subscriber Server", function () {
     const eventID = "5678";
     const eventData = { data: "data" };
 
-    subscriber.saveEvent(dataStoreFake, subID, eventID, eventData);
+    subscriberManager.saveEvent(subID, eventID, eventData);
     expect(dataStoreFake.eventDatabase.length).to.equal(1);
   });
 
   it("should return a list of events.", function () {
-    const events = subscriber.getAllEvents(
-      dataStoreFake,
-      subscriptionRecordStub.id
-    );
+    const events = subscriberManager.getAllEvents(subscriptionRecordStub.id);
     expect(events).to.include.deep.members([eventRecordStub]);
   });
 
   it("should removed a subscription.", function () {
-    expect(subscriber.removeSubscription(dataStoreFake, "12345")).to.equal(
-      "Removed."
-    );
+    expect(subscriberManager.removeSubscription("12345")).to.equal("Removed.");
   });
 
   it("should return a list of latest events.", function () {
-    const events = subscriber.getLatestEvents(
-      dataStoreFake,
-      subscriptionRecordStub.id
-    );
+    const events = subscriberManager.getLatestEvents(subscriptionRecordStub.id);
     expect(events).to.include.deep.members(eventRecordListStub);
   });
 });
